a64eb22d20adfe85c14d25cba4bf79f6981d3330
naufal <naufal.razzan@hotmail.com>
2024-05-03 02:11:43 +0700

    changes req.ip to req.socket.remoteAddress

--- src/module/order-movie/order-movie.controller.ts
+++ src/module/order-movie/order-movie.controller.ts
@@ -39,7 +39,7 @@ export class OrderMovieController {
 
         const totalTime = afterTime - beforeTime
 
-        this.logger.log(`${req.socket.remoteAddresscket.remoteAddress} HTTP/:${req.httpVersion} ${req.headers['user-agent']} - ${HttpStatus.OK} ${req.method} ${req.url} '${msg}' ${Buffer.byteLength(JSON.stringify(result))} bytes ${totalTime} ms`)
+        this.logger.log(`${req.socket.remoteAddress} HTTP/:${req.httpVersion} ${req.headers['user-agent']} - ${HttpStatus.OK} ${req.method} ${req.url} '${msg}' ${Buffer.byteLength(JSON.stringify(result))} bytes ${totalTime} ms`)
 
         return result        
     }
